cd22a5992676043b8a8b860d6b6e542d7ba9faff
velyaevolegoo2-ux <velyaev.olegoo2@gmail.com>
2026-02-19 19:16:09 -0800

    Update __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,1 +1,15 @@
 """Routes package for Hep"""
+
+from .orders import router as orders_router
+from .translate import router as translate_router
+from .telegram import router as telegram_router
+from .simple_auth import router as simple_auth_router
+from .rephrase import router as rephrase_router
+
+__all__ = [
+    "orders_router",
+    "translate_router", 
+    "telegram_router",
+    "simple_auth_router",
+    "rephrase_router"
+]
